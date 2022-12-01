af1d0e14c4622141bdee438357913e01c6aa1e3c
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-12-01 23:51:38 +0530

    UC4_EvenBinding

--- src/App.js
+++ src/App.js
@@ -3,18 +3,25 @@ import './App.css';
 import logo from './assets/img/bridgelabz.png'
 
 class App extends React.Component{
-
+  url="https://www.bridgelabz.com";
   constructor(){
     super()
     this.state={
       title:'Hello from Bridgelab'
     }
   }
+
+//onClick function
+onClick=($event) => {
+  console.log("save button is clicked",$event);
+  window.open(this.url,"_blank");
+}
+
   render(){ 
     return(
       <div className='App'>
       <h1>{this.state.title}</h1>
-      <img src={logo}
+      <img src={logo} onClick={this.onClick}
       alt="The bridgelabz logo: a bridge to Employee lab works"/>
       </div>
     );
