e57f587b03ac6084c6970a1afcce13025098b6b9
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-12-02 00:45:21 +0530

    UC6_UserNameUsingRegexVlidation

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends React.Component{
   constructor(){
     super()
     this.state={
-      userName:''
+      userName:'',
+      nameError:''
     }
   }
 
@@ -19,22 +20,29 @@ onClick=($event) => {
 
 onNameChange=(event) =>{
   console.log("value is ", event.target.value);
+  const nameRegex= RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
   //set thetitle using setState method
   this.setState({userName: event.target.value})
-}
+  if(nameRegex.test(event.target.value)){
+    this.setState({nameError:''})
+  }else{
+    this.setState({nameError:'Name is Incorrect'})
+  }
+  }
 
   render(){ 
     return(
-      <>
-      <div >
+      <div>
+      <div>
       <h1>Hello {this.state.userName} from Bridgelabz</h1>
       <img src={logo} onClick={this.onClick}
       alt="The bridgelabz logo: a bridge to Employee lab works"/>
       </div>
       <div>
         <input onChange={this.onNameChange} />
+        <span className="error-output">{this.state.nameError}</span>
       </div>
-      </>
+      </div>
     );
   }
 }
